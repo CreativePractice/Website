86124e87face2af8ce3f5e89a07236572e1bdd13
vale3760 <vale3760@gmail.com>
2021-03-16 20:33:14 -0400

    added navbar just frontend
    we might want to put the navbar in  a components folder.

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+
+import "bootswatch/dist/lumen/bootstrap.min.css";
+import { Navbar, Nav, Button,Form,FormControl} from "react-bootstrap";
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Creative Practices</Navbar.Brand>
+        <Nav className="mr-auto active">
+          <Nav.Link href="#home">About Us</Nav.Link>
+          <Nav.Link href="#ContactUs">Contact Us</Nav.Link>
+          <Nav.Link href="#Calendar">Calendar</Nav.Link>
+          <Nav.Link href="#Performances">Performances</Nav.Link>
+        </Nav>
+      </Navbar>
+  
+    </div>
+  );
+}
+
+export default App;

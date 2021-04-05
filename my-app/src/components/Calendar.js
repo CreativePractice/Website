c4609bdd0550f0c608368f807cc9053d53b10138
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-05 12:59:17 -0400

    Update Calendar.js

--- my-app/src/components/Calendar.js
+++ my-app/src/components/Calendar.js
@@ -4,7 +4,7 @@ const Calendar = () => {
     return (
         <div>
            <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Calendar</h1>
-           <iframe title="Club Events" src="https://calendar.google.com/calendar/embed?src=66cmarp45lidor56j4gvt322a4%40group.calendar.google.com&ctz=America%2FNew_York" containerStyle="border: 0" width="1100" height="700" frameborder="0" scrolling="no"></iframe>
+           <iframe title="Club Events" src="https://calendar.google.com/calendar/embed?src=66cmarp45lidor56j4gvt322a4%40group.calendar.google.com&ctz=America%2FNew_York" containerStyle="border: 0" width="100%" height="700" frameborder="0" scrolling="no"></iframe>
         </div>
     )
 }

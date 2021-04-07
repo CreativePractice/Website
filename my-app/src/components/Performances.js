e288770a265533e016a2e39426469584616fb354
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-06 22:19:37 -0400

    Create Performances.js

--- my-app/src/components/Performances.js
+++ my-app/src/components/Performances.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Performances = () => {
     return (
         <div>
-           <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Performances</h1> 
+            <br></br>
+           <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Performances</h1>
+            <br></br>
+            <br></br>
+           <iframe width="100%" height="625px" src="https://www.youtube.com/embed/videoseries?list=PLVfzM_81kRplg3O-j_YcB5v2mo49_iOAb" title="Club Performances" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <br></br>
+            <br></br>
         </div>
     )
 }

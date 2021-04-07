dd20c70ab8cb33d7b1b05c58baa7df6a0a608066
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-06 22:21:22 -0400

    Update Calendar.js

--- my-app/src/components/Calendar.js
+++ my-app/src/components/Calendar.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Calendar = () => {
     return (
         <div>
+            <br></br>
            <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Calendar</h1>
            <iframe title="Club Events" src="https://calendar.google.com/calendar/embed?src=66cmarp45lidor56j4gvt322a4%40group.calendar.google.com&ctz=America%2FNew_York" containerStyle="border: 0" width="100%" height="700" frameborder="0" scrolling="no"></iframe>
         </div>

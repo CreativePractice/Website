cdb94b71c2df80afec7296e6d5e3301c3b97f38a
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2021-04-07 15:44:22 -0400

    Update Performances.js

--- my-app/src/components/Performances.js
+++ my-app/src/components/Performances.js
@@ -7,7 +7,7 @@ const Performances = () => {
            <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Performances</h1>
             <br></br>
             <br></br>
-           <iframe width="100%" height="625px" src="https://www.youtube.com/embed/videoseries?list=PLVfzM_81kRplg3O-j_YcB5v2mo49_iOAb" title="Club Performances" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <iframe width="100%" height="625px" src="https://www.youtube.com/embed/videoseries?list=PL-J1eHouY6DUszb7JyIsEBKBcPOwn5IFv" title="Club Performances" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <br></br>
             <br></br>
         </div>

1b8004bb17dafb4cbdb5734377824684aaf792c0
Sebastian Faure <sebastianfaure@ufl.edu>
2021-04-10 00:01:50 -0400

    Added facebook footer button and club leadership photos.

--- my-app/src/components/Calendar.js
+++ my-app/src/components/Calendar.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import img from "../images/FacebookIcon.png";
+
+function myfunction() {
+    var win = window.open("https://www.facebook.com/groups/387221978540976/", '_blank');
+    if (win != null) {
+        win.focus();
+      }
+}
 
 const Calendar = () => {
     return (
@@ -6,7 +14,18 @@ const Calendar = () => {
             <br></br>
            <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Calendar</h1>
            <iframe title="Club Events" src="https://calendar.google.com/calendar/embed?src=66cmarp45lidor56j4gvt322a4%40group.calendar.google.com&ctz=America%2FNew_York" containerStyle="border: 0" width="100%" height="700" frameborder="0" scrolling="no"></iframe>
+           <br></br>
+           <br></br>
+           <br></br>
+           <br></br>
+           <footer>
+                <div style={{textAlignVertical: "center",textAlign: "center"}}>
+                    <button style={{height: "5.5%", width: "5.5%", border: "none", backgroundColor:"transparent"}}><img style={{height: "100%", width: "100%"}} src={img} alt="icon" onClick={myfunction} /></button>
+                </div>
+            </footer>
+            <br></br>
         </div>
+        
     )
 }
 
--- my-app/src/components/Performances.js
+++ my-app/src/components/Performances.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
+import img from "../images/FacebookIcon.png";
+
+function myfunction() {
+    var win = window.open("https://www.facebook.com/groups/387221978540976/", '_blank');
+    if (win != null) {
+        win.focus();
+      }
+}
+
 const Performances = () => {
     return (
         <div>
             <br></br>
            <h1 style={{textAlignVertical: "center",textAlign: "center", color: "#158cba", fontSize: 50}}>Performances</h1>
-            <br></br>
-            <br></br>
+           <br></br>
+           <br></br>
+           <br></br>
+           <br></br>
            <iframe width="100%" height="625px" src="https://www.youtube.com/embed/videoseries?list=PL-J1eHouY6DUszb7JyIsEBKBcPOwn5IFv" title="Club Performances" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <br></br>
+           <br></br>
+           <br></br>
+           <br></br>
+           <br></br>
+            <footer>
+                <div style={{textAlignVertical: "center",textAlign: "center"}}>
+                    <button style={{height: "5.5%", width: "5.5%", border: "none", backgroundColor:"transparent"}}><img style={{height: "100%", width: "100%"}} src={img} alt="icon" onClick={myfunction} /></button>
+                </div>
+            </footer>
             <br></br>
         </div>
     )

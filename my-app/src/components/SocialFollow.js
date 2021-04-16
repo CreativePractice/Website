e460decc3866b26f3f6f8fe17eedfa7b5d1d9da8
Michael Ivanov <ivanovmichael@ufl.edu>
2021-04-16 15:35:50 -0400

    made social links open in new tab

--- my-app/src/components/SocialFollow.js
+++ my-app/src/components/SocialFollow.js
@@ -14,15 +14,15 @@ function SocialFollow() {
       <div class="social-container">
         <h3 style={{ textAlignVertical: "center", textAlign: "center" }}>Connect with us</h3>
         <Row className="justify-content-center">
-        <a href="https://www.youtube.com/channel/UCKg-lFILwy3LS5XP9zajE7A"
+        <a href="https://www.youtube.com/channel/UCKg-lFILwy3LS5XP9zajE7A" target="_blank" rel="noopener noreferrer"
           className="youtube social">
           <FontAwesomeIcon icon={faYoutube} size="2x" />
         </a>
-        <a href="https://www.facebook.com/groups/387221978540976/"
+        <a href="https://www.facebook.com/groups/387221978540976/" target="_blank" rel="noopener noreferrer"
           className="facebook social">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
-        <a href="https://mcc.med.ufl.edu/organizations/cpr/"
+        <a href="https://mcc.med.ufl.edu/organizations/cpr/" target="_blank" rel="noopener noreferrer"
           className="web social">
           <FontAwesomeIcon icon={ faGlobe } size="2x" />
         </a>
